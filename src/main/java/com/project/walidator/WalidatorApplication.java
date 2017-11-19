d97dd05903da75e6602726d4dfac5d75411d57fd
Łukasz Macuga <lmacuga@spyro-soft.com>
2017-11-19 16:52:52 +0100

    Spring application

---
+++ src/main/java/com/project/walidator/WalidatorApplication.java
@@ -0,0 +1,12 @@
+package com.project.walidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalidatorApplication.class, args);
+	}
+}
